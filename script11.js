89c03ffdb52f7763cd944dd73ffc7fd8af47106d
Mackedonec <mackedonec187@gmail.com>
2025-11-20 16:43:43 +0200

    upd

--- script11.js
+++ script11.js
@@ -159,3 +159,200 @@ console.log(getIndexToIns([10, 20, 30, 40, 50], 35));
 console.log(getIndexToIns([10, 20, 30, 40, 50], 30));
 
 // -------------------------------------------------
+
+let diffArray = function (arr1 = [], arr2 = []) {
+  let newArr = [
+    ...arr1.filter((item) => !arr2.includes(item)),
+    ...arr2.filter((item) => !arr1.includes(item)),
+  ];
+  return newArr;
+};
+
+console.log(
+  diffArray(
+    ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"],
+    ["andesite", "grass", "dirt", "dead shrub"]
+  )
+);
+
+// -------------------------------------------------
+
+let destroyer = function (arr, ...args) {
+  let filteredArr = arr.filter((item) => !args.includes(item));
+  return filteredArr;
+};
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+console.log(
+  destroyer(
+    [
+      "possum",
+      "trollo",
+      12,
+      "safari",
+      "hotdog",
+      92,
+      65,
+      "grandma",
+      "bugati",
+      "trojan",
+      "yacht",
+    ],
+    "yacht",
+    "possum",
+    "trollo",
+    "safari",
+    "hotdog",
+    "grandma",
+    "bugati",
+    "trojan"
+  )
+);
+
+// ---------------------------------
+
+let whatIsInAName = function (arr, obj) {
+  let keys = Object.keys(obj);
+  let newArr = arr.filter((item) => {
+    return keys.every((key) => {
+      return item.hasOwnProperty(key) && item[key] === obj[key];
+    });
+  });
+  return newArr;
+};
+
+// let whatIsInAName = (arr, obj) =>
+//   arr.filter((item) => Object.keys(obj).every((key) => item[key] === obj[key]));
+
+console.log(
+  whatIsInAName(
+    [
+      { first: "Romeo", last: "Montague" },
+      { first: "Mercutio", last: null },
+      { first: "Tybalt", last: "Capulet" },
+    ],
+    { last: "Capulet" }
+  )
+);
+
+console.log(
+  whatIsInAName(
+    [
+      { apple: 1, bat: 2 },
+      { apple: 1 },
+      { apple: 1, bat: 2, cookie: 2 },
+      { bat: 2 },
+    ],
+    { apple: 1, bat: 2 }
+  )
+);
+
+console.log(
+  whatIsInAName([{ a: 1, b: 2, c: 3, d: 9999 }], { a: 1, b: 9999, c: 3 })
+);
+
+// ---------------------------------
+
+let sumPrimes = function (num) {
+  let sum = 0;
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      sum += i;
+    }
+  }
+  function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return sum;
+};
+
+console.log(sumPrimes(10));
+console.log(sumPrimes(977));
+
+// ---------------------------------
+
+let smallestCommons = function (arr) {
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let intMax = max + 1;
+
+  while (true) {
+    if (isDivisibleByAll(intMax, min, max)) {
+      return intMax;
+    }
+    intMax++;
+  }
+  function isDivisibleByAll(num, min, max) {
+    for (let i = min; i <= max; i++) {
+      if (num % i !== 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+console.log(smallestCommons([1, 5]));
+console.log(smallestCommons([5, 1]));
+console.log(smallestCommons([2, 10]));
+console.log(smallestCommons([1, 13]));
+
+// ---------------------------------
+
+let steamrollArray = function (arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      newArr = newArr.concat(steamrollArray(arr[i]));
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
+console.log(steamrollArray(steamrollArray([1, {}, [3, [[4]]]])));
+
+// ---------------------------------
+
+let truthCheck = function (arr, prop) {
+  return arr.every((obj) => Boolean(obj[prop]));
+};
+
+console.log(
+  truthCheck(
+    [
+      { name: "Quincy", role: "Founder", isBot: false },
+      { name: "Naomi", role: "", isBot: false },
+      { name: "Camperbot", role: "Bot", isBot: true },
+    ],
+    "isBot"
+  )
+);
+
+console.log(
+  truthCheck(
+    [
+      {
+        id: 1,
+        data: { url: "https://freecodecamp.org", name: "freeCodeCamp" },
+      },
+      {
+        id: 2,
+        data: { url: "https://coderadio.freecodecamp.org/", name: "CodeRadio" },
+      },
+      { id: null, data: {} },
+    ],
+    "data"
+  )
+);
+
+// ---------------------------------
+
