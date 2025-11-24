2ed875e4e8f3155f568382039ac6af0ac4847d69
Mackedonec <mackedonec187@gmail.com>
2025-11-24 17:19:05 +0200

    upd

--- DOM/script1.js
+++ DOM/script1.js
@@ -2,7 +2,7 @@ const storyContainer = document.querySelector(".story-container");
 const scaryStoryBtn = document.getElementById("scary-btn");
 const funnyStoryBtn = document.getElementById("funny-btn");
 const adventureStoryBtn = document.getElementById("adventure-btn");
-const mysteryStoryBtn = document.getElementById("mystery-btn");
+// const mysteryStoryBtn = document.getElementById("mystery-btn");
 const resultParagraph = document.getElementById("result");
 
 let storyObj = {
@@ -10,21 +10,21 @@ let storyObj = {
     story:
       "In the dark woods, a group of friends stumbled upon an old, abandoned cabin. They enter the cabin and awaken something malevolent that had been dormant for centuries.",
     borderColor: "#ee4b2b",
-    color: "#ee4b2b",
+    // color: "#ee4b2b",
   },
 
   funny: {
     story:
       "During a camping trip, Mark decided to show off his culinary skills by cooking dinner over an open fire. However, his attempt caused him to burn the dinner as well as his eyebrows off.",
     borderColor: "#f1be32",
-    color: "#f1be32",
+    // color: "#f1be32",
   },
 
   adventure: {
     story:
       "Lost in the heart of the Amazon rain forest, Sarah and Jake stumbled upon an ancient temple. They braved deadly traps and encountered strange wildlife, all while deciphering cryptic clues left behind by a mysterious civilization.",
     borderColor: "#acd157",
-    color: "#acd157",
+    // color: "#acd157",
   },
 };
 
@@ -32,24 +32,24 @@ let displayStory = function (genre) {
   if (storyObj[genre]) {
     resultParagraph.textContent = storyObj[genre].story;
     storyContainer.style.borderColor = storyObj[genre].borderColor;
-    resultParagraph.style.color = storyObj[genre].color;
+    // resultParagraph.style.color = storyObj[genre].color;
   }
 };
 
-let addNewStory = function (genre, story, borderColor, color, btn) {
-  storyObj[genre] = {
-    story: story,
-    borderColor: borderColor,
-    color: color,
-  };
-};
+// let addNewStory = function (genre, story, borderColor, color) {
+//   storyObj[genre] = {
+//     story: story,
+//     borderColor: borderColor,
+//     color: color,
+//   };
+// };
 
-addNewStory(
-  "mystery",
-  "In the quiet town of Ravenwood, a series of unexplained disappearances has left the community on edge. Detective Laura Collins is determined to uncover the truth behind the eerie occurrences, leading her down a path filled with secrets and suspense.",
-  "#8e44ad",
-  "#8e44ad"
-);
+// addNewStory(
+//   "mystery",
+//   "In the quiet town of Ravenwood, a series of unexplained disappearances has left the community on edge. Detective Laura Collins is determined to uncover the truth behind the eerie occurrences, leading her down a path filled with secrets and suspense.",
+//   "#8e44ad",
+//   "#8e44ad"
+// );
 
 scaryStoryBtn.addEventListener("click", function () {
   displayStory("scary");
@@ -63,9 +63,9 @@ adventureStoryBtn.addEventListener("click", function () {
   displayStory("adventure");
 });
 
-mysteryStoryBtn.addEventListener("click", function () {
-  displayStory("mystery");
-});
+// mysteryStoryBtn.addEventListener("click", function () {
+//   displayStory("mystery");
+// });
 
 // const storyContainer = document.querySelector(".story-container");
 // const scaryStoryBtn = document.getElementById("scary-btn");
